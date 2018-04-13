32f13aecec7cc1f1173289e5f376f17cf86b9e49
Mike Pennisi <mike@mikepennisi.com>
2018-04-12 21:44:38 -0400

    PARTIAL: Begin to fix
    This patch fixes support for Edge on Windows 10, but Safari on macOS
    continues to fail `/infrastructure/assumptions/ahem.html`

--- fonts/README.md
+++ fonts/README.md
@@ -1,2 +1,6 @@
 This directory only contains auxiliary font files used by other tests. See
 /css-fonts for tests covering the CSS Fonts Module specification.
+
+The font named `Ahem.ttf` is referenced from the project documentation and the
+CLI's scripts for provisioning Microsoft Windows and Apple macOS. If that file
+is re-located, those references should be updated accordingly.
--- tools/wptrunner/wptrunner/browsers/sauce_setup/safari-prerun.sh
+++ tools/wptrunner/wptrunner/browsers/sauce_setup/safari-prerun.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaScriptCanOpenWindowsAutomatically -bool true
+
+# Download and install the Ahem font
+# - https://wiki.saucelabs.com/display/DOCS/Downloading+Files+to+a+Sauce+Labs+Virtual+Machine+Prior+to+Testing
+# - https://apple.stackexchange.com/questions/240381/installing-fonts-from-terminal-instead-of-font-book
+#curl -o /Library/Fonts/Ahem.ttf 'https://raw.githubusercontent.com/w3c/web-platform-tests/master/fonts/Ahem.ttf'
+curl -o ~/Library/Fonts/Ahem.ttf 'https://raw.githubusercontent.com/w3c/web-platform-tests/master/fonts/Ahem.ttf'
