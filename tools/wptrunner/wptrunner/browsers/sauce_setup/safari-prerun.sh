d2ae5d1bd7cb3bd79a557db85db2f7c9e0e71de4
Mike Pennisi <mike@mikepennisi.com>
2018-04-16 19:44:24 -0400

    continue debugging

--- tools/wptrunner/wptrunner/browsers/sauce_setup/safari-prerun.sh
+++ tools/wptrunner/wptrunner/browsers/sauce_setup/safari-prerun.sh
@@ -8,32 +8,13 @@ defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebK
 # - https://wiki.saucelabs.com/display/DOCS/Downloading+Files+to+a+Sauce+Labs+Virtual+Machine+Prior+to+Testing
 # - https://apple.stackexchange.com/questions/240381/installing-fonts-from-terminal-instead-of-font-book
 #curl -o /Library/Fonts/Ahem.ttf 'https://raw.githubusercontent.com/w3c/web-platform-tests/master/fonts/Ahem.ttf'
-curl -o Ahem.ttf 'https://raw.githubusercontent.com/w3c/web-platform-tests/master/fonts/Ahem.ttf'
-echo Curl result: $?
-ls -lah .
 
 bash -c '
 set -x
 
-whoami
-pwd
-groups
-ls /Users
-which sudo
-ls -lah ~
-touch /Users/vagrant/tmp
-ls -lah /Users/vagrant/tmp
-ls -lah ~/Library/Fonts
+curl -o Ahem.ttf "https://raw.githubusercontent.com/w3c/web-platform-tests/master/fonts/Ahem.ttf"
 
-echo "162.222.75.243 www.google.com" >> /etc/hosts
-cat /etc/hosts
-
-ls -lah .
-mv Ahem.ttf /Library/Fonts/
 mkdir -p ~/Library/Fonts
-cp Ahem.ttf ~/Library/Fonts/
-ls -lah /Users/Shared
-mkdir -p /Users/Shared/Library/Fonts
-cp Ahem.ttf /Users/Shared/Library/Fonts/
-ls -lah /Users/Shared
+mv Ahem.ttf ~/Library/Fonts/
+ls -lah ~/Library/Fonts/
 ' 2>&1
