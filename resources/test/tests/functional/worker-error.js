be58932621363dff561128742b973b053c45754b
Mike Pennisi <mike@mikepennisi.com>
2018-07-10 10:57:07 -0400

    fixup! [testharness.js] Report error for worker errors

--- resources/test/tests/functional/worker-error.js
+++ resources/test/tests/functional/worker-error.js
@@ -1,7 +1,7 @@
 importScripts("/resources/testharness.js");
 
 // The following sub-test ensures that the worker is not interpreted as a
-// single-page test.  The subsequent uncaught exception should therefor be
+// single-page test.  The subsequent uncaught exception should therefore be
 // interpreted as a harness error rather than a single-page test failure.
 test(function() {}, "worker test that completes successfully before exception");
 
