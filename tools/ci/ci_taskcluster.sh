63873aa9f31fec217060ad46a1bea68194fa1d88
Mike Pennisi <mike@mikepennisi.com>
2018-07-18 15:41:20 -0400

    Reduce output rate still further

--- tools/ci/ci_taskcluster.sh
+++ tools/ci/ci_taskcluster.sh
@@ -3,7 +3,7 @@
 ./wpt manifest-download
 
 if [ $1 == "firefox" ]; then
-    ./wpt run firefox --log-wptreport=../artifacts/wpt_report.json --log-mach=- --log-mach-level=info --this-chunk=$3 --total-chunks=$4 --test-type=$2 -y --install-browser --no-pause --no-restart-on-unexpected --reftest-internal --install-fonts --no-fail-on-unexpected --include encoding | python ./tools/ci/throttle-output.py 50
+    ./wpt run firefox --log-wptreport=../artifacts/wpt_report.json --log-mach=- --log-mach-level=info --this-chunk=$3 --total-chunks=$4 --test-type=$2 -y --install-browser --no-pause --no-restart-on-unexpected --reftest-internal --install-fonts --no-fail-on-unexpected --include encoding | python ./tools/ci/throttle-output.py 10
 elif [ $1 == "chrome" ]; then
     ./wpt run chrome --log-tbpl=../artifacts/log_tbpl.log --log-tbpl-level=info --log-wptreport=../artifacts/wpt_report.json --log-mach=- --this-chunk=$3 --total-chunks=$4 --test-type=$2 -y  --no-pause --no-restart-on-unexpected --install-fonts --no-fail-on-unexpected
 fi
