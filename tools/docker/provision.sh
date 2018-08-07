f7f3019013fc57993af6b3043d5e9158e689df3b
Mike Pennisi <mike@mikepennisi.com>
2018-08-07 17:24:53 -0400

    [tools] Generalize repository cloning procedure
    This simplification is intended to support a future extension to
    validate pull requests.

--- tools/docker/provision.sh
+++ tools/docker/provision.sh
@@ -12,23 +12,17 @@
 set -ex
 
 REMOTE=${1:-https://github.com/web-platform-tests/wpt}
-BRANCH=${2:-master}
-REV=${3:-FETCH_HEAD}
-BROWSER=${4:-all}
+REF=${2:-master}
+BROWSER=${3:-all}
 
 cd ~
 
-# Initially we just fetch 50 commits in order to save several minutes of fetching
-git clone ${REMOTE} --single-branch --branch ${BRANCH} --no-checkout -q --depth=50 web-platform-tests
+mkdir web-platform-tests
 cd web-platform-tests
-if [[ ! `git rev-parse --verify -q ${REV}` ]];
-then
-    # But if for some reason the commit under test isn't in that range, we give in and
-    # fetch everything
-    git fetch -q --unshallow ${REMOTE}
-    git rev-parse --verify ${REV}
-fi
-git checkout -b build ${REV}
+git init
+git remote add origin ${REMOTE}
+git fetch --depth 1 origin ${REF}
+git checkout FETCH_HEAD
 
 sudo sh -c './wpt make-hosts-file >> /etc/hosts'
 
