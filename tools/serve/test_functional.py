817c10be2476554636c0490776318e8d4b608122
Mike Pennisi <mike@mikepennisi.com>
2018-08-17 13:26:44 -0400

    [serve] Quit on sub-process failure
    The correctness of the WPT server is dependent on the availability of
    all server sub-processes. If any sub-process dies, the server is no
    longer capable of performing its role. Allow the process to exit in
    this case.
    
    Introduce a test for this behavior.

---
+++ tools/serve/test_functional.py
@@ -0,0 +1,60 @@
+import json
+import os
+import Queue
+import tempfile
+import threading
+
+import pytest
+
+from . import serve
+
+
+class StubServerProc(serve.ServerProc):
+    instances = None
+
+    def __init__(self, *args, **kwargs):
+        super(StubServerProc, self).__init__(*args, **kwargs)
+
+        if StubServerProc.instances is not None:
+            StubServerProc.instances.put(self)
+
+serve.ServerProc = StubServerProc
+
+@pytest.fixture()
+def server_subprocesses():
+    StubServerProc.instances = Queue.Queue()
+    yield StubServerProc.instances
+    StubServerProc.instances = None
+
+@pytest.fixture()
+def tempfile_name():
+    name = tempfile.mkstemp()[1]
+    yield name
+    os.remove(name)
+
+
+def test_subprocess_exit(server_subprocesses, tempfile_name):
+    timeout = 10
+
+    def target():
+        # By default, the server initially creates a child to validate local
+        # system configuration. That process is unrelated to the behavior under
+        # test, but it currently uses the same constructor. Disable the check
+        # so that the constructor is only used to create the long-running
+        # processes which are relevant to this functionality.
+        with open(tempfile_name, 'w') as handle:
+            json.dump({"check_subdomains": False}, handle)
+
+        serve.run(config_path=tempfile_name)
+
+    thread = threading.Thread(target=target)
+
+    thread.start()
+
+    server_subprocesses.get(True, timeout)
+    subprocess = server_subprocesses.get(True, timeout)
+    subprocess.kill()
+
+    thread.join(timeout)
+
+    assert not thread.is_alive()
