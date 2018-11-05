bce525e2c5b072405a1835cbba5919786ecad716
Mike Pennisi <mike@mikepennisi.com>
2018-11-05 15:35:24 -0500

    Implement simple fix

--- tools/wptrunner/wptrunner/executors/testharness_webdriver_resume.js
+++ tools/wptrunner/wptrunner/executors/testharness_webdriver_resume.js
@@ -1,3 +1,5 @@
 var callback = arguments[arguments.length - 1];
-window.opener.testdriver_callback = callback;
+window.opener.testdriver_callback = function(results) {
+  callback(JSON.parse(JSON.stringify(results)));
+};
 window.opener.process_next_event();
