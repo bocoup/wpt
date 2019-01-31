eaa3d211f493f17e26ea49a85c6d88c7dc9259b5
Mike Pennisi <mike@mikepennisi.com>
2019-01-31 14:53:22 -0500

    Refactor dependency organization

--- tools/pyppeteer/setup.py
+++ tools/pyppeteer/setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
 
+deps = ["six>=1.8", "lomond==0.3.2"]
+
 setup(name="pyppeteer",
       version="0.1",
       description="WebDriver client compatible with "
@@ -8,7 +10,7 @@ setup(name="pyppeteer",
       author_email="public-test-infra@w3.org",
       license="BSD",
       packages=find_packages(),
-      install_requires=["lomond"],
+      install_requires=deps,
       classifiers=["Development Status :: 4 - Beta",
                    "Intended Audience :: Developers",
                    "Operating System :: OS Independent"])
