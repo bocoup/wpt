36ac2103a65ea6a1da59ef2758e8bc828e21b41c
Mike Pennisi <mike@mikepennisi.com>
2019-03-13 20:42:14 -0400

    Introduce search feature for documentation site
    This search is provided by DuckDuckGo.

--- docs/assets/menu.js
+++ docs/assets/menu.js
@@ -1,6 +1,7 @@
 (function() {
   var site_nav = document.querySelector(".wpt-site-nav");
   var trigger = document.querySelector(".wpt-site-nav .trigger");
+  var icon = document.querySelector(".menu-icon");
 
   var show = function(e) {
     trigger.setAttribute("aria-hidden", "false");
@@ -15,6 +16,26 @@
   site_nav.addEventListener("focus", show, false);
   site_nav.addEventListener("blur", hide_if_relatedTarget_elsewhere, true);
 
-  site_nav.addEventListener("mouseenter", show, false);
+  icon.addEventListener("mouseenter", show, false);
   site_nav.addEventListener("mouseleave", hide_if_relatedTarget_elsewhere, false);
+
+  /**
+   * Intercept form submissions to ensure the search query is scoped to the
+   * current domain.
+   */
+  var handle_submit = function(e) {
+    e.preventDefault();
+    var query = e.target.querySelector("[name=q]").value;
+
+    if (!/\bsite:/.test(query)) {
+      query += " site:" + location.origin;
+    }
+
+    location.href = "https://duckduckgo.com/?q=" + encodeURIComponent(query);
+
+    return false;
+  };
+
+  document.querySelector("#site-search")
+    .addEventListener("submit", handle_submit);
 })();
