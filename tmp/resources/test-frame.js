04dcf5525665d04b9b2449509739dfb1c055ecf8
Mike Pennisi <mike@mikepennisi.com>
2021-04-21 15:02:28 -0400

    Make a giant mess

--- tmp/resources/test-frame.js
+++ tmp/resources/test-frame.js
@@ -1,17 +1,26 @@
 'use strict';
 
-parent.postMessage('child running', '*');
+(opener || parent).postMessage('child running', '*');
 
 addEventListener('message', async (event) => {
 	if (!event.data) {
 		return;
 	}
-	if (event.data === 'back') {
+	if (event.data.name === 'back') {
+		console.log('going back');
 		history.back();
-	} else if (event.data === 'readReferer') {
-		parent.postMessage(await readReferer(window), '*');
-	} else if (event.data[0] === 'createMeta') {
-		createMeta(window, event.data[1], event.data[2]);
-		parent.postMessage('meta created', '*');
+	} else if (event.data.name === 'readReferer') {
+		parent.postMessage({
+			id: event.data.id,
+			value: await readReferer(window)
+		}, '*');
+	} else if (event.data.name === 'createMeta') {
+		createMeta(window, ...event.data.values);
+		parent.postMessage({
+			id: event.data.id,
+			value: 'meta created'
+		}, '*');
+	} else if (event.data.name === 'navigate') {
+		location = event.data.values[0];
 	}
 });
