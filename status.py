b29e4c22028bdef36fb611b23c994b06e969183d
Mike Pennisi <mike@mikepennisi.com>
2025-09-24 11:01:54 -0400

    Improve progress script

--- status.py
+++ status.py
@@ -21,25 +21,27 @@ def find():
 def main(progress_filename):
     all_classified = find()
     classified = set()
-    skipped = set()
+    unclassifiable = set()
     unclassified = set()
 
     with open(progress_filename, 'r') as progress_file:
         reader = csv.reader(progress_file)
         for row in reader:
             if row[2]:
-                skipped.add(row[0])
+                unclassifiable.add(row[0])
             elif row[0] in all_classified:
                 classified.add(row[0])
             else:
                 unclassified.add(row[0])
 
-    total = len(classified) + len(skipped) + len(unclassified)
+    total = len(classified) + len(unclassifiable) + len(unclassified)
 
-    print(f'Classified: {len(classified)}')
-    print(f'Skipped: {len(skipped)}')
-    print(f'Unclassified: {len(unclassified)}')
-    print(f'Total: {total}')
+    print('Classified:        {:3d}'.format(len(classified)))
+    print('Unclassifiable:    {:3d}'.format(len(unclassifiable)))
+    print('Unclassified:    + {:3d}'.format(len(unclassified)))
+    print('                 -----');
+    print('Total:             {}'.format(total))
+    print('Progress:          {:2.0f}%'.format(100 * (1 - len(unclassified)/total)))
 
 if __name__ == '__main__':
     main('./progress.csv')
